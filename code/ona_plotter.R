74fc49a186019da7c0cfa17148c2c2f0db1a75b1
ashiqwisc <ashiq@wisc.edu>
2023-03-29 20:36:44 -0500

    Model code changed, models built
    Made new function to model the ONA plot of Quan vs. Non-Quan. Ran all 6 different combinations of unit values, saved images in /images folder.

--- code/ona_plotter.R
+++ code/ona_plotter.R
@@ -116,3 +116,60 @@ make.ona.plot.compare.two.students <- function(set,
   print(sub)
 }
 
+
+make.ona.plot.compare.two.cases <- function(set, 
+                                            plot.name
+                                            # node_size_multiplier, 
+                                            # edge_size_multiplier
+){
+  color <- c("red", "blue")
+  case_Quan <- plot(set, title = "ONA plot for Quan") %>%
+    edges(
+      weights = set$line.weights$cases$Quan,
+      edge_color = c("red")) %>%
+    nodes(
+      # node_size_multiplier = node_size_multiplier,
+      self_connection_color = c("red")) %>%
+    units(
+      points=set$points[set$points$cases == "Quan",],
+      points_color = c("red"),
+      show_mean = FALSE, show_points = TRUE, with_ci = FALSE) 
+  print(case_Quan)
+  
+  case_non_Quan <- plot(set, title = "ONA plot for non-Quan") %>%
+    edges(
+      weights = set$line.weights$cases$`Non-Quan`,
+      edge_color = c("blue")) %>%
+    nodes(
+      # node_size_multiplier = node_size_multiplier,
+      self_connection_color = c("blue")) %>%
+    units(
+      points=set$points[set$points$cases == "Non-Quan",],
+      points_color = c("blue"),
+      show_mean = FALSE, show_points = TRUE, with_ci = FALSE)
+  print(case_non_Quan)
+  
+  # Difference plot
+  sub <- plot(set, title = "Difference: Quan (red) vs non-Quan (blue)") %>%
+    units(
+      points = set$points$cases$Quan, 
+      points_color = "red",
+      show_mean = TRUE, show_points = TRUE, with_ci = FALSE) %>%
+    units(
+      points = set$points$cases$`Non-Quan`, 
+      points_color = "blue",
+      show_mean = TRUE, show_points = TRUE, with_ci = FALSE) %>%
+    edges(
+      weights = (colMeans(set$line.weights$cases$Quan) - colMeans(set$line.weights$cases$`Non-Quan`)),
+      edge_size_multiplier = 5,
+      edge_arrow_saturation_multiplier = 2,
+      # node_position_multiplier = node_position_multiplier,
+      edge_color = c("red","blue")) %>%
+    nodes(
+      # node_size_multiplier = 2,
+      # node_position_multiplier = node_position_multiplier,
+      self_connection_color = c("red","blue"))
+  print(sub)
+}
+
+
