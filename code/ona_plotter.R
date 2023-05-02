98221f03a9d9d2489c1ac194140192f95ed6af4a
ashiqwisc <ashiq@wisc.edu>
2023-05-02 08:58:28 -0500

    Plots and such

--- code/ona_plotter.R
+++ code/ona_plotter.R
@@ -1,18 +1,20 @@
 library(dplyr)
-library(rstatix)
 make.ona.plot.six.pairs <- function(set, 
                           plot.name
                           # node_size_multiplier, 
                           # edge_size_multiplier
                           ){
   color <- c("red", "orange", "green", "blue", "purple", "grey")
-  plot_all_pairs <- plot(set, title = paste0(plot.name," (all pairs)")) %>%
+  plot_all_pairs <- plot(set, title = paste0("Grand mean ",plot.name)) %>%
     edges(
-      weights =set$line.weights,
+      # weights =set$line.weights,
+      weights =set$line.weights[set$line.weights$Participant == "Rakshya" | set$line.weights$Participant == "Rakshya",],
       edge_color = c("black")) %>%
     nodes(
+      node_labels = FALSE,
       node_size_multiplier = 0.5,
-      self_connection_color = c("grey")) 
+      self_connection_color = c("grey") 
+      ) 
     # units(
     #   points=set$points[set$points$Patient.Student.Pair == "Lucas & Rakshya" & set$points$model_unit == TRUE,],
     #   points_color = c("red"),
@@ -68,7 +70,7 @@ make.ona.plot.compare.two.students <- function(set,
                           # edge_size_multiplier
 ){
   color <- c("red", "blue")
-  student_Rakshya <- plot(set, title = "ONA plot for Rakshya") %>%
+  student_Rakshya <- plot(set, title = "ONA plot for Roshini") %>%
     edges(
       weights = set$line.weights$Participant$Rakshya,
       edge_color = c("red")) %>%
@@ -81,7 +83,7 @@ make.ona.plot.compare.two.students <- function(set,
       show_mean = TRUE, show_points = TRUE, with_ci = TRUE) 
   print(student_Rakshya)
   
-  student_Ryleigh <- plot(set, title = "ONA plot for Ryleigh") %>%
+  student_Ryleigh <- plot(set, title = "ONA plot for Rose") %>%
     edges(
       weights = set$line.weights$Participant$Ryleigh,
       edge_color = c("blue")) %>%
@@ -95,7 +97,7 @@ make.ona.plot.compare.two.students <- function(set,
   print(student_Ryleigh)
   
   # FirstGame and SecondGame subtracted plot
-  sub <- plot(set, title = "Difference: Rakshya (red) vs Ryleigh (blue)") %>%
+  sub <- plot(set, title = "Difference: Roshini (red) vs Rose (blue)") %>%
     units(
       points = set$points$Participant$Rakshya, 
       points_color = "red",
@@ -116,16 +118,6 @@ make.ona.plot.compare.two.students <- function(set,
       self_connection_color = c("red","blue"))
   print(sub)
   print(t.test(set$points$Participant$Rakshya$MR1, set$points$Participant$Ryleigh$MR1))
-  data_rakshya <- as.data.frame(set$points$Participant$Rakshya$MR1) %>% 
-    mutate(points = `set$points$Participant$Rakshya$MR1`, ID = "Rakshya") %>%
-    select(-`set$points$Participant$Rakshya$MR1`)
-  print(data_rakshya)
-  data_ryleigh <- as.data.frame(set$points$Participant$Ryleigh$MR1) %>%
-    mutate(points = `set$points$Participant$Ryleigh$MR1`, ID = "Ryleigh") %>%
-    select(-`set$points$Participant$Ryleigh$MR1`)
-  data <- rbind(data_rakshya, data_ryleigh)
-  # Effect sizes for each pairwise t test
-  print(cohens_d(data, points ~ ID))
 }
 
 make.ona.plot.compare.two.cases <- function(set, 
@@ -140,6 +132,7 @@ make.ona.plot.compare.two.cases <- function(set,
       weights = set$line.weights[set$line.weights$cases == "Quan" & set$line.weights$model_unit == TRUE,],
       edge_color = c("orange")) %>%
     nodes(
+      node_labels = FALSE,
       node_size_multiplier = 0.5,
       self_connection_color = c("orange")) %>%
     units(
@@ -155,6 +148,7 @@ make.ona.plot.compare.two.cases <- function(set,
       weights = set$line.weights[set$line.weights$cases == "Non-Quan" & set$line.weights$model_unit == TRUE,],
       edge_color = c("purple")) %>%
     nodes(
+      node_labels = FALSE,
       node_size_multiplier = 0.5,
       self_connection_color = c("purple")) %>%
     units(
@@ -184,20 +178,11 @@ make.ona.plot.compare.two.cases <- function(set,
       # node_position_multiplier = node_position_multiplier,
       edge_color = c("orange","purple")) %>%
     nodes(
+      node_labels = FALSE,
       node_size_multiplier = 0.5,
       # node_position_multiplier = node_position_multiplier,
       self_connection_color = c("orange", "purple"))
       # self_connection_color = c("orange", "purple"))
   print(sub)
   print(t.test(set$points[set$points$cases == "Quan" & set$points$model_unit == TRUE,]$MR1, set$points[set$points$cases == "Non-Quan" & set$points$model_unit == TRUE,]$MR1))
-  data_quan <- as.data.frame(set$points[set$points$cases == "Quan" & set$points$model_unit == TRUE,]$MR1) %>% 
-    mutate(points = set$points[set$points$cases == "Quan" & set$points$model_unit == TRUE,]$MR1, ID = "Quan") %>%
-    select(-c(1))
-  print(data_quan)
-  data_non_quan <- as.data.frame(set$points[set$points$cases == "Non-Quan" & set$points$model_unit == TRUE,]$MR1) %>%
-    mutate(points = set$points[set$points$cases == "Non-Quan" & set$points$model_unit == TRUE,]$MR1, ID = "Non-Quan") %>%
-    select(-c(1))
-  data <- rbind(data_quan, data_non_quan)
-  # Effect sizes for each pairwise t test
-  print(cohens_d(data, points ~ ID))
 }
